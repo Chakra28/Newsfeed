fdd3ac193d4c42a57aea635a65ff2320d6e8741b
Chakra <chakrakotilnka28@gmail.com>
2018-02-09 15:50:27 +0530

    login

--- src/app.module.js
+++ src/app.module.js
@@ -2,9 +2,10 @@ import './less/styles.less';
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import routes from './app.routes';
-import desk from './desk';
-
-angular.module('MyApp', [uirouter, desk])
+import homeComponent from './home';
+import aboutComponent from './about';
+import loginComponent from "./login";
+angular.module('newsfeed', [uirouter, homeComponent, aboutComponent,loginComponent])
     .config(routes);
 
 if (process.env.NODE_ENV !== 'production') {
